ae7ef9446f0c4d340c76c4d1ce94def1451199a8
Pinfo14 <emanuazaz09@gmail.com>
2024-02-06 20:45:41 +0000

    engine caput

--- equipment.c
+++ equipment.c
@@ -13,10 +13,65 @@
 #include "equipment.h"
 #include "input.h"
 #include "stdio.h"
-
 #include "search.h"
 #include "memory.h"
 
+/**
+ * @brief Message prompt for asking the user to input a designation.
+ */
+#define DESIGNATION_MSG "Designation: "
+
+/**
+ * @brief Message prompt for asking the user to input a day.
+ */
+#define DAY_MSG "Day: "
+
+/**
+ * @brief Message prompt for asking the user to input a month.
+ */
+#define MONTH_MSG "Month: "
+
+/**
+ * @brief Message prompt for asking the user to input a year.
+ */
+#define YEAR_MSG "Year: "
+
+/**
+ * @brief Message prompt for asking the user to input a category.
+ */
+#define CATG_MSG "Category: "
+
+/**
+ * @brief Message prompt for asking the user to input a state.
+ */
+#define STATE_MSG "State: "
+
+/**
+ * @brief Message prompt for asking the user to insert an ID.
+ */
+#define ID_MSG "Insert the id: "
+
+/**
+ * @brief Message prompt for notes.
+ */
+#define NOTES "NOTES: "
+
+/**
+ * @brief Error message indicating an error occurred.
+ */
+#define ERRO "ERROO"
+void printCatg(){
+    printf("/--------------------CATEGORIES-----------------/\n");
+    printf("1. Printer\n2.Desktop\n3.Laptop\n4.Monitor\n5.Router\n5.Switch\n6.TV\n7.Projector\n8.Access_controller\n9.Mouse\n10.Accessory\n11.Other\n\n");
+}
+void printState(){
+    printf("/--------------------STATE-----------------/\n");
+    printf(" 0.Non operational\n1.In Maintenance\n2.Operational\n3.Recycling");
+}
+void printMainCatg(){
+    printf("/--------------------MAINTENANCE CATEGORY-----------------/\n");
+    printf(" 1.SoftwareUpdate\n2.Technique\n3.CompSubstitution");
+}
 void insertEquipments(Equipments *equipments){
     reallocEquipment(equipments);
     int lastId=0;
@@ -27,12 +82,14 @@ void insertEquipments(Equipments *equipments){
     }
     equipments->equipment[equipments->count].id = lastId+1;
 
-    readString(equipments->equipment[equipments->count].designation,MAX_CHAR,"Designation: ");
-    equipments->equipment[equipments->count].date.day = getInt(MIN_DAY,MAX_DAY,"Day: ");
-    equipments->equipment[equipments->count].date.month = getInt(MIN_MONTH,MAX_MONTH,"Month: ");
-    equipments->equipment[equipments->count].date.year = getInt(MIN_YEAR,MAX_YEAR,"Year: ");
-    equipments->equipment[equipments->count].category = getInt(MIN_CATG,MAX_CATG,"category: ");
-    equipments->equipment[equipments->count].state = getInt(MIN_CATG,MAX_CATG,"state: ");
+    readString(equipments->equipment[equipments->count].designation,MAX_CHAR,DESIGNATION_MSG);
+    equipments->equipment[equipments->count].date.day = getInt(MIN_DAY,MAX_DAY,DAY_MSG);
+    equipments->equipment[equipments->count].date.month = getInt(MIN_MONTH,MAX_MONTH,MONTH_MSG);
+    equipments->equipment[equipments->count].date.year = getInt(MIN_YEAR,MAX_YEAR,YEAR_MSG);
+    printCatg();
+    equipments->equipment[equipments->count].category = getInt(MIN_CATG,MAX_CATG,CATG_MSG);
+    printState();
+    equipments->equipment[equipments->count].state = getInt(MIN_CATG,MAX_CATG,STATE_MSG);
     equipments->count++;
 }
 
@@ -41,7 +98,7 @@ void addMaintenanceToEquipment(Equipments *equipments) {
     int id = 0;
     int position =-1;
     do {
-        id = getInt(MIN_ID,MAX_ID,"Insert the id: ");
+        id = getInt(MIN_ID,MAX_ID,ID_MSG);
         position = searchEq(*equipments,id);
     }while(position==-1);
 
@@ -49,16 +106,16 @@ void addMaintenanceToEquipment(Equipments *equipments) {
     if (equipments->equipment[position].maintenance != NULL) {
             // Add the new maintenance record
             equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].num_movement++;
-            equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].date.day= getInt(MIN_DAY,MAX_DAY,"DAY: ");
-            equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].date.month= getInt(MIN_MONTH,MAX_MONTH,"MONTH: ");
-            equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].date.year= getInt(MIN_YEAR,MAX_YEAR,"year: ");
-            equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].category= getInt(MIN_DAY,MAX_DAY,"ccatg: ");
-            readString( equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].notes,MAX_CHAR,"NOTES: ");
+            equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].date.day= getInt(MIN_DAY,MAX_DAY,DAY_MSG);
+            equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].date.month= getInt(MIN_MONTH,MAX_MONTH,MONTH_MSG);
+            equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].date.year= getInt(MIN_YEAR,MAX_YEAR,YEAR_MSG);
+            printMainCatg();
+            equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].category= getInt(MIN_DAY,MAX_DAY,CATG_MSG);
+            readString( equipments->equipment[position].maintenance[equipments->equipment[position].num_maintenance].notes,MAX_CHAR,NOTES);
             equipments->equipment[position].num_maintenance++;
 
-            printf("Maintenance added successfully.\n");
         } else {
-            printf("Error: Memory allocation failed.\n");
+        puts(ERRO);
         }
 
 
@@ -69,11 +126,11 @@ void updateState(Equipments *equipments){
     int position =0;
     if(equipments->count>0) {
         do {
-            id = getInt(MIN_ID, MAX_ID, "Insert the id: ");
+            id = getInt(MIN_ID, MAX_ID, ID_MSG);
             position = searchEq(*equipments, id);
         } while (position == -1);
 
-        equipments->equipment[position].state = getInt(1, 4, "State: ");
+        equipments->equipment[position].state = getInt(1, 4, STATE_MSG);
     }
 }
 
@@ -83,7 +140,7 @@ void displayMaintenanceHistory( Equipments *equipments) {
     int position =0;
 
         do {
-            id = getInt(MIN_ID, MAX_ID, "Insert the id: ");
+            id = getInt(MIN_ID, MAX_ID, ID_MSG);
             position = searchEq(*equipments, id);
         } while (position == -1);
     if(equipments->equipment[position].num_maintenance>0) {
@@ -116,7 +173,7 @@ void deleteEquipment(Equipments *equipments) {
 
     if (equipments->count > 0) {
         do {
-            id = getInt(MIN_ID, MAX_ID, "Insert the id: ");
+            id = getInt(MIN_ID, MAX_ID, ID_MSG);
             position = searchEq(*equipments, id);
         } while (position == -1);
 
